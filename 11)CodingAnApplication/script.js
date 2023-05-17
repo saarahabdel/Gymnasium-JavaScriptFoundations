23d9f26b01508bc5b65474628e4f7d0782ba51cd
saarahabdel <saarahabdel15@gmail.com>
2023-05-18 00:50:05 +0200

    Attempted Assignment 3 (CodingApplication)

--- 11)CodingAnApplication/script.js
+++ 11)CodingAnApplication/script.js
@@ -1,3 +1,29 @@
+/** Number three, in the sample application we created, we’re
+validating each numeric property and setting the border
+style of the input related with it. Try creating a new
+function in that project called “validateinput.” It should take a
+number value in a text input element as parameters.
+Now, fill in the body of that function, so that it performs the
+same action as the original code. Replace each original if/
+else statement with a call to that new function. See
+how this makes the overall code smaller, and easier to read.
+*/
+
+// function validateInput(value, input) {
+
+//     if(isNaN(value)) {
+
+//         input = "red";
+//         return;
+//     }
+//     else {
+//         input = "initial";
+//     }
+// }
+// function isNumber(n) {
+//     return !isNaN(parseFloat(n)) && !isNaN(n - 0)
+//   }
+  
 var distanceInput = document.getElementById("distance"),
     hoursInput = document.getElementById("hours"),
     minutesInput = document.getElementById("minutes"),
@@ -7,43 +33,28 @@ var distanceInput = document.getElementById("distance"),
 
 calculateBtn.addEventListener("click", function() {
 
+    function validateInput(value, input) {
+
+        if(isNaN(value)) {
+    
+            input.style.borderColor = "red";
+            return;
+        }
+        else {
+            input.style.borderColor = "initial";
+        }
+    }
+    validateInput();
+
     var miles = parseFloat(distanceInput.value),
         hours = parseFloat(hoursInput.value),
         minutes = parseFloat(minutesInput.value),
         seconds = parseFloat(secondsInput.value);
         
         // added to check if a user input words instead of a number value
-        if(isNaN(miles)) {
-            distanceInput.style.borderColor = "red";
-            return;
-        }
-        else {
-            distanceInput.style.borderColor = "initial";
-        }
-
-        if(isNaN(hours)) {
-            hoursInput.style.borderColor = "red";
-            return;
-        }
-        else {
-            hoursInput.style.borderColor = "initial";
-        }
-
-        if(isNaN(minutes)) {
-            minutesInput.style.borderColor = "red";
-            return;
-        }
-        else {
-            minutesInput.style.borderColor = "initial";
-        }
-
-        if(isNaN(seconds)) {
-            secondsInput.style.borderColor = "red";
-            return;
-        }
-        else {
-            secondsInput.style.borderColor = "initial";
-        }
+        
+        
+        
 
     var totalMinutes = hours * 60 + minutes + seconds / 60,
         pace = totalMinutes / miles;
